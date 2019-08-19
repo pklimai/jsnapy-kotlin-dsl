e5979d5cd160c7a376c9e3b22679668cc23f7443
Peter Klimai <pklimai@gmail.com>
2019-08-20 01:01:46 +0300

    Further tweak model to make list elements display

--- src/main/kotlin/pklimai/jsnapykotlindsl/lib/JSNAPyTest.kt
+++ src/main/kotlin/pklimai/jsnapykotlindsl/lib/JSNAPyTest.kt
@@ -1,51 +1,48 @@
 package pklimai.jsnapykotlindsl.lib
 
-// Represents a single JSNAPy test
-@YAMLElementMarker
-class JSNAPyTest() {
+open class JSNAPyTestListElements()
 
+class ListElementRPCorCommand: JSNAPyTestListElements() {
     // Either command or rpc, not both
     var command: String? = null
     var rpc: String? = null
     // kwargs is only for rpc
     val kwargs = linkedMapOf<String, String>() // Use linked map to keep insertion order
-    fun kwarg(key: String, value: String) {
-        kwargs[key] = value
-    }
+}
 
+class ListElementItemORIterate: JSNAPyTestListElements() {
     var item: JSNAPyItem? = null
-    fun item(block: JSNAPyItem.() -> Unit) {
-        item = JSNAPyItem().apply(block)
-    }
-
     var iterate: JSNAPyIterate? = null
-    fun iterate(block: JSNAPyIterate.() -> Unit) {
-        iterate = JSNAPyIterate().apply(block)
+}
+
+// Represents a single JSNAPy test
+@YAMLElementMarker
+class JSNAPyTest: ArrayList<JSNAPyTestListElements>() {
+    init {
+        add(ListElementRPCorCommand())
+        add(ListElementItemORIterate())
     }
 
-//    override fun toString() = buildString {
-//        append("$name:\n")
-//        if (rpc != null && command == null) {
-//            append(" - rpc: $rpc\n")
-//            if (kwargs.isNotEmpty()) {
-//                append("   kwargs:\n")
-//                kwargs.forEach { append("     ${it.key}: ${it.value}\n") }
-//            }
-//        }
-//        else if (rpc == null && command != null) {
-//            append(" - command: $command\n")
-//            if (kwargs.isNotEmpty()) terminate("'kwargs' is only for 'rpc'")
-//        }
-//        else terminate("'rpc' XOR 'command' must be present (not both)")
-//
-//        if (item != null && iterate == null) {
-//            append(item.toString())
-//        }
-//        else if (item == null && iterate != null) {
-//            append(iterate.toString())
-//        }
-//        else if (item != null && iterate != null) {
-//            terminate("'item' OR 'iterate' must be present (but not both)")
-//        }
-//    }
+    var command: String? = null
+        set(value) {
+            (this[0] as ListElementRPCorCommand).command = value
+        }
+
+    var rpc: String? = null
+        set(value) {
+            (this[0] as ListElementRPCorCommand).rpc = value
+        }
+
+    fun kwarg(key: String, value: String) {
+        (this[0] as ListElementRPCorCommand).kwargs[key] = value
+    }
+
+    fun item(block: JSNAPyItem.() -> Unit) {
+        (this[1] as ListElementItemORIterate).item = JSNAPyItem().apply(block)
+    }
+
+    fun iterate(block: JSNAPyIterate.() -> Unit) {
+        (this[1] as ListElementItemORIterate).iterate = JSNAPyIterate().apply(block)
+    }
+
 }
